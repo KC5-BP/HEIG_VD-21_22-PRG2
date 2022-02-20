e514f96139f2dee5665717ed290ebdad37a04c14
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-02-20 20:12:29 +0100

    Copy trees from HEIG_VD-20_21-PRG2-Exos repo.

---
+++ base.c
@@ -0,0 +1,21 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : <nom du fichier>.<x> (x = h ou c)
+ Auteur(s)      : <pr�nom> <nom>
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : <� compl�ter>
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 8.1.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    // <code>
+    return EXIT_SUCCESS;
+}

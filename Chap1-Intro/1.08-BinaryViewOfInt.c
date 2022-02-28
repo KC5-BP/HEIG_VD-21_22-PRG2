6a594ffbba45e29f3056a3e0a3cfb3df5046efaa
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-02-28 15:11:51 +0100

    CMakeLists : Updated with all exo1.xx | 1.08 Done

--- Chap1-Intro/1.08-BinaryViewOfInt.c
+++ Chap1-Intro/1.08-BinaryViewOfInt.c
@@ -10,7 +10,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BIT_SIZE_32_BITS (sizeof(int32_t) * 8)
+
+void decimalToBinary(int32_t n, int8_t binary[]) {
+	for (int i = 0; i < BIT_SIZE_32_BITS; ++i)
+		// [0, 1, ... , BIT-2, BIT-1] : MSB to LSB
+		//binary[i] = (n & (1 << (BIT_SIZE_32_BITS-i-1))) >> (BIT_SIZE_32_BITS-i-1);
+		// [0, 1, ... , BIT-2, BIT-1] : LSB to MSB
+		binary[i] = n >> i & 1;
+}
+void printBinary(int8_t binary[]) {
+	printf("0b");
+	for (int i = 0; i < BIT_SIZE_32_BITS; ++i) {
+		if (!(i % 4) && (i != 0))
+			printf(" ");
+		//printf("%d", binary[i]);
+		printf("%d", binary[BIT_SIZE_32_BITS-i-1]);
+	}
+	printf("\n");
+}
+
 int main(void) {
-	// <code>
+	int8_t binary[BIT_SIZE_32_BITS] = {0};
+
+	for(int32_t n = 0; n < 50; n+=5) {
+		printf("n = %2d : ", n);
+		decimalToBinary(n, binary);
+		printBinary(binary);
+	}
+
 	return EXIT_SUCCESS;
 }

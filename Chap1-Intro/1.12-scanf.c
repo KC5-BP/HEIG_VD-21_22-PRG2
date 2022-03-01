bb2070233e3f19e9e1e308dbe0388a577d763aa3
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-01 14:45:49 +0100

    1.12 Kind of done!

--- Chap1-Intro/1.12-scanf.c
+++ Chap1-Intro/1.12-scanf.c
@@ -13,11 +13,17 @@ int main() {
     return EXIT_SUCCESS;
 }
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	// <code>
+int main() {
+	int n;
+	char c;
+
+	printf("Donnez un nombre entier et un caractere : ");
+	scanf("%d%c", &n, &c);
+
+	printf("n = %d, c = %c\n", n, c);
+
 	return EXIT_SUCCESS;
 }

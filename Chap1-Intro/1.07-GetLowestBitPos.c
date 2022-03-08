4a0c255eaff3a7602873c584d37b4cec07b687a5
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-08 17:11:34 +0100

    1.07 : Correction about invert condition for the while in fn. + Set an array for test printing.

--- Chap1-Intro/1.07-GetLowestBitPos.c
+++ Chap1-Intro/1.07-GetLowestBitPos.c
@@ -14,13 +14,14 @@
 short lowestOrderSetBit(int n) {
    if (n == 0) return (-1);
    short pos = 0;
-   while (n >> pos & 1)
-      ++pos;
+   while ( !(n >> pos & 1) )  ++pos;
    return pos;
 }
 
 int main(void) {
-   int n = 5;
-   printf("First bit set is at : %d\n", lowestOrderSetBit(n));
+   int array[] = {0, 1, 2, 4, 8, 16, 32, 64, 5, 48};
+   for (int i = 0; i < sizeof(array) / sizeof(int); ++i)
+      printf("First bit set of %2d is at : %d\n", \
+                                    array[i], lowestOrderSetBit(array[i]));
 	return EXIT_SUCCESS;
 }

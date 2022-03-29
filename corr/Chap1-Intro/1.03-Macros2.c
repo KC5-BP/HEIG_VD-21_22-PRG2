8e3bbdf49d6f0c9ecc9cb999e5102f65a8bb8e1f
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-29 15:10:58 +0200

    1.03 Clean (maybe come back on this one)

--- corr/Chap1-Intro/1.03-Macros2.c
+++ corr/Chap1-Intro/1.03-Macros2.c
@@ -1,81 +1,113 @@
-// Exo 1.3 : Donnee ;
-//                  1) Que va afficher le programme suivant
-//                  2) Expliquez en quoi le programme est problematique ?
-//                  3) Est-il possible de recrire (sans utiliser la
-//                      fonction abs de stdlib) la macro ABS de maniere
-//                      a eviter les problemes du pt2 ?
 /*
-#include <stdio.h>
-#include <stdlib.h>
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 1.03-Macros2.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
 
-#define ABS(X) (((X) > 0) ? (X) : (-X))
+ Description    : 1.03
+                     1) Que va afficher le programme suivant
+                     2) Expliquez en quoi le programme est problematique ?
+                     3) Est-il possible de recrire (sans utiliser la
+                        fonction abs de stdlib) la macro ABS de maniere
+                        a eviter les problemes du pt2 ?
 
-int main(void) {
-   int n;
+ Remarque(s)    : <� compl�ter>
 
-   printf("1. %d\n", ABS(2));
-   printf("2. %d\n", ABS(-2));
-   printf("3. %d\n", ABS('A'));
-
-   n = -2;
-   printf("4. %d\n", ABS(n+1));
-
-   n = -2;
-   printf("5. %d", ABS(n++)); printf(" %d\n", n);
-
-   n = -2;
-   printf("6. %d", ABS(++n)); printf(" %d\n", n);
-
-   n = -2;
-   printf("7. %d", abs(++n)); printf(" %d\n", n);
-
-   return EXIT_SUCCESS;
-}
- */
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ABS(X) (((X) > 0) ? (X) : (-X))
+// Uncomment to see the correct output after pt3 has been done.
+//#define PT3_CORRECTED 0
+#ifndef PT3_CORRECTED
 
-int main(void) {
-	int n;
+      #define PT2_CORRECTED 0
+      #ifndef PT2_CORRECTED
+         #define ABS(X) (((X) > 0) ? (X) : (-X))
+      #else
+         #define ABS(X) (((X) > 0) ? (X) : -(X))
+      #endif   // END PT2_CORRECTED
 
-	printf("1. %d\n", ABS(2));
-	// 1) Will print 2
-	printf("2. %d\n", ABS(-2));
-	// 1) Will print 2
-	printf("3. %d\n", ABS('A'));
-	// 1) Will print 65
+   int main(void) {
+      int n;
 
-	n = -2;
-	printf("4. %d\n", ABS(n+1));
-	// (((n+1) > 0) ? (n+1) : (-n+1))
-	// 1) Will print 3
+      printf("1. %d\n", ABS(2));
+      // 1) Will print 2
+      printf("2. %d\n", ABS(-2));
+      // 2) Will print 2
+      printf("3. %d\n", ABS('A'));
+      // 3) Will print 65
 
-	n = -2;
-	printf("5. %d", ABS(n++)); printf(" %d\n", n);
-	// (((n++) > 0) ? (n++) : (-n++))
-	// After test : -1
-	//      Will print 1 for first %d
-	//          Will print 0 for second %d because of post increase before.
-	// 1) 1 0
+      n = -2;
+      printf("4. %d\n", ABS(n+1));
+      // (((n+1) > 0) ? (n+1) : (-n+1))
+      // 4) Will print 3
 
-	n = -2;
-	printf("6. %d", ABS(++n)); printf(" %d\n", n);
-	// (((++n) > 0) ? (++n) : (-++n))
-	// After test : -1
-	//      Will print 0 for first %d because pre increase and negate after.
-	//          Will print 0 for second %d.
-	// 1) 0 0
+      n = -2;
+      printf("5. %d", ABS(n++)); printf(" %d\n", n);
+      // (((n++) > 0) ? (n++) : (-n++))
+      // After test : -1
+      //      Will print 1 for first %d
+      //          Will print 0 for second %d because of post increase before.
+      // 5) 1 0
+
+      n = -2;
+      printf("6. %d", ABS(++n)); printf(" %d\n", n);
+      // (((++n) > 0) ? (++n) : (-++n))
+      // After test : -1
+      //      Will print 0 for first %d because pre increase and negate after.
+      //          Will print 0 for second %d.
+      // 6) 0 0
 
 
-	n = -2;
-	printf("7. %d", abs(++n)); printf(" %d\n", n);
-	// Will print 1 -1 by using abs from stdlib
+      n = -2;
+      printf("7. %d", abs(++n)); printf(" %d\n", n);
+      // 7) Will print 1 -1 by using abs from stdlib
 
-	return EXIT_SUCCESS;
-}
+      return EXIT_SUCCESS;
+   }
+
+#else // PT3 defined
+
+   #define GENERIC_ABS(type)					\
+   type abs_##type(type input) {				\
+      return input < 0 ? -input : input;	\
+   }
+   GENERIC_ABS(int)
+
+   int main(void) {
+      int n;
+
+      printf("1. %d\n", abs_int(2));
+      // 1) Will print 2
+      printf("2. %d\n", abs_int(-2));
+      // 2) Will print 2
+      printf("3. %d\n", abs_int('A'));
+      // 3) Will print 65
+
+      n = -2;
+      printf("4. %d\n", abs_int(n+1));
+      // 4) Will print 1
+
+      n = -2;
+      printf("5. %d", abs_int(n++)); printf(" %d\n", n);
+      // 5) 2 -1
+
+      n = -2;
+      printf("6. %d", abs_int(++n)); printf(" %d\n", n);
+      // 6) 1 -1
+
+      n = -2;
+      printf("7. %d", abs(++n)); printf(" %d\n", n);
+      // 7) 1 -1
+
+      return EXIT_SUCCESS;
+   }
+#endif   // END PT3_CORRECTED
+
 /*
  * 2) - Multiple increase instead of a simple one
  *                   ("Side effect" / "Effet de bord" on 5 & 6) .

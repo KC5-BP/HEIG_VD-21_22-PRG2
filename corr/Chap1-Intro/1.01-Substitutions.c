4c09ed6ec6903867d129dda3b422d23d0e1ff7ca
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-29 14:25:48 +0200

    1.01 Clean

--- corr/Chap1-Intro/1.01-Substitutions.c
+++ corr/Chap1-Intro/1.01-Substitutions.c
@@ -1,23 +1,18 @@
-// Exo 1.1 : Donnee ;
-//                   Indiquer ce que va afficher le code suivant :
 /*
-#include <stdio.h>
-#include <stdlib.h>
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 1.01-Substitutions.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
 
-#define A 1
-#define B 2
-#define AB 3
-#define F(X,Y) X##Y
+ Description    : Exo 1.1
+                     Indiquer ce que va afficher le code suivant
 
-int main(void) {
-   int n = AB;
-   printf("%<?>\n", n);
-   printf("%<?>\n", "AB");
-   printf("%<?>\n", F(A, B));
+ Remarque(s)    : <� compl�ter>
 
-   return EXIT_SUCCESS;
-}
- */
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
--- template/Chap1-Intro/1.01-Substitutions.c
+++ template/Chap1-Intro/1.01-Substitutions.c
@@ -1,5 +1,18 @@
-// Exo 1.1 : Donnee ;
-//                   Indiquer ce que va afficher le code suivant ..
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 1.01-Substitutions.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : Exo 1.1
+                     Indiquer ce que va afficher le code suivant
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 

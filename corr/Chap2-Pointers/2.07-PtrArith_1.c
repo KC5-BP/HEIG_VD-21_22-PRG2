9db3e627f5bb434b0a63ad020cb1433aa868f918
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-29 23:41:28 +0200

    2.07 Clean (still need to check the answer for the resolution)

--- corr/Chap2-Pointers/2.07-PtrArith_1.c
+++ corr/Chap2-Pointers/2.07-PtrArith_1.c
@@ -1,10 +1,11 @@
 /*
  -----------------------------------------------------------------------------------
- Nom du fichier : <nom du fichier>.<x> (x = h ou c)
- Auteur(s)      : K. BOUGNON-PEIGNE
+ Nom du fichier : 2.07-PtrArith_1.c
+ Auteur(s)      : (\_/)
  Date creation  : <jj.mm.aaaa>
 
- Description    : <� compl�ter>
+ Description    : 2.07
+                     Que va afficher le main ci-dessous ?
 
  Remarque(s)    : <� compl�ter>
 
@@ -12,10 +13,47 @@
  -----------------------------------------------------------------------------------
 */
 
-#include <stdio.h>
+#include <inttypes.h>
+#include <stddef.h> // pour ptrdiff_t
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(void) {
-	// <code>
-	return EXIT_SUCCESS;
+   int a[] = {10, 20, 30, 40, 50};
+   int* p[] = {a, a+1, a+2, a+3, a+4};
+   int** pp = &p[2];
+   int*** ppp = &pp;
+
+   printf("1) %d\n", *a);     // 1) 10
+   printf("2) %d\n", **p);    // 2) 10
+   printf("3) %d\n", **pp++); // 3) 30
+                              // But now, pp point on &p[3]
+   printf("4) %d\n", ***ppp); // 4) 40
+   pp = p;
+   printf("5) %d\n", **++pp); // 5) 20
+                              // pp => &p[0]
+                              // ++pp => &p[1]
+                              // *++pp => a + 1
+                              // **++pp => 20
+   pp = p;
+   printf("6) %d\n", ++**pp); // 6) 11
+                              // pp => &p[0]
+                              // *pp => a
+                              // **pp => 10
+                              // ++**pp => 11
+   pp = p;
+   printf("7) %d\n", *pp[1]); // 7) 20
+                              // pp => &p[0]
+                              // pp[1] => a + 1
+                              // *pp[1] => 20
+   pp = p;
+   printf("8) %" PRIdPTR "\n", (ptrdiff_t) (*(p+1) - *pp)); // 8) 1
+                                                            // *(p+1) <=> p[1]
+                                                            //             => a + 1
+                                                            //
+                                                            // pp => &p[0]
+                                                            // *pp => a
+                                                            // a + 1 - a = 1
+
+   return EXIT_SUCCESS;
 }
--- template/Chap2-Pointers/2.07-PtrArith_1.c
+++ template/Chap2-Pointers/2.07-PtrArith_1.c
@@ -1,10 +1,11 @@
 /*
  -----------------------------------------------------------------------------------
- Nom du fichier : <nom du fichier>.<x> (x = h ou c)
- Auteur(s)      : K. BOUGNON-PEIGNE
+ Nom du fichier : 2.07-PtrArith_1.c
+ Auteur(s)      : (\_/)
  Date creation  : <jj.mm.aaaa>
 
- Description    : <� compl�ter>
+ Description    : 2.07
+                     Que va afficher le main ci-dessous ?
 
  Remarque(s)    : <� compl�ter>
 
@@ -12,10 +13,29 @@
  -----------------------------------------------------------------------------------
 */
 
-#include <stdio.h>
+#include <inttypes.h>
+#include <stddef.h> // pour ptrdiff_t
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(void) {
-	// <code>
-	return EXIT_SUCCESS;
+   int a[] = {10, 20, 30, 40, 50};
+   int* p[] = {a, a+1, a+2, a+3, a+4};
+   int** pp = &p[2];
+   int*** ppp = &pp;
+
+   printf("1) %d\n", *a);
+   printf("2) %d\n", **p);
+   printf("3) %d\n", **pp++);
+   printf("4) %d\n", ***ppp);
+   pp = p;
+   printf("5) %d\n", **++pp);
+   pp = p;
+   printf("6) %d\n", ++**pp);
+   pp = p;
+   printf("7) %d\n", *pp[1]);
+   pp = p;
+   printf("8) %" PRIdPTR "\n", (ptrdiff_t) (*(p+1) - *pp));
+
+   return EXIT_SUCCESS;
 }

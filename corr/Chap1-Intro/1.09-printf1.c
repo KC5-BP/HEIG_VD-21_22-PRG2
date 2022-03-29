ba9199384af23d264af8a74a761baeb49bdae38b
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-29 16:21:11 +0200

    1.09 Clean

--- corr/Chap1-Intro/1.09-printf1.c
+++ corr/Chap1-Intro/1.09-printf1.c
@@ -1,25 +1,17 @@
-// Exo 1.9 : Donnee ;
-//                  Qu'affiche les lignes suivantes :
 /*
-#include <stdio.h>
-#include <stdlib.h>
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 1.09-printf1.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
 
- int main(void) {
-	int n = 127;
-	double x = 12.3456;
+ Description    : 1.09
+                     Qu'affiche les lignes suivantes ..
 
-	printf("R1 : |%-4d|\n", n);
-	printf("R2 : |%04d|\n", n);
-	printf("R3 : |%x|\n", n);
-	printf("R4 : |%#o|\n", n);
-	printf("R5 : |%f|\n", x);
-	printf("R6 : |%5.2f|\n", x);
-	printf("R7 : |%.2e|\n", x);
-	printf("R8 : |%g|\n", x);
+ Remarque(s)    : <� compl�ter>
 
-	return EXIT_SUCCESS;
-}
- */
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
--- template/Chap1-Intro/1.09-printf1.c
+++ template/Chap1-Intro/1.09-printf1.c
@@ -1,10 +1,22 @@
-// Exo 1.9 : Donnee ;
-//                  Qu'affiche les lignes suivantes :
 /*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 1.09-printf1.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 1.09
+                     Qu'affiche les lignes suivantes ..
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
- int main(void) {
+int main(void) {
 	int n = 127;
 	double x = 12.3456;
 
@@ -16,26 +28,6 @@
 	printf("R6 : |%5.2f|\n", x);
 	printf("R7 : |%.2e|\n", x);
 	printf("R8 : |%g|\n", x);
-
-	return EXIT_SUCCESS;
-}
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-	int n = 127;
-	double x = 12.3456;
-
-	printf("R1 : |%-4d|\n", n);   // |127 |
-	printf("R2 : |%04d|\n", n);   // |0127|
-	printf("R3 : |%x|\n", n);     // |7f|
-	printf("R4 : |%#o|\n", n);    // |0177|
-	printf("R5 : |%f|\n", x);     // |12.345600|
-	printf("R6 : |%5.2f|\n", x);  // |12.35|
-	printf("R7 : |%.2e|\n", x);   // |1.23e+01|
-	printf("R8 : |%g|\n", x);     // |12.3456|
 
 	return EXIT_SUCCESS;
 }

514396534c7ac2366b230ad912b5d3e83699392b
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-29 21:37:00 +0200

    2.03 Clean (still need to check the answer)

--- template/Chap2-Pointers/2.03-FnWithPtrAsReturn.c
+++ template/Chap2-Pointers/2.03-FnWithPtrAsReturn.c
@@ -1,10 +1,11 @@
 /*
  -----------------------------------------------------------------------------------
- Nom du fichier : <nom du fichier>.<x> (x = h ou c)
- Auteur(s)      : K. BOUGNON-PEIGNE
+ Nom du fichier : 2.03-FnWithPtrAsReturn.c
+ Auteur(s)      : (\_/)
  Date creation  : <jj.mm.aaaa>
 
- Description    : <� compl�ter>
+ Description    : 2.03
+                     Que va produire a l'execution le programme ci dessous
 
  Remarque(s)    : <� compl�ter>
 
@@ -15,7 +16,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	// <code>
-	return EXIT_SUCCESS;
+int* carres(const int tab[]);
+
+int main() {
+   int tab[] = {1, 2, 3};
+   const size_t N = sizeof(tab) / sizeof(int);
+   int* ptr = carres(tab);
+
+   for(size_t i = 0; i < N; ++i)
+      printf("%d ", ptr[i]);
+
+   printf("\n");
+   return EXIT_SUCCESS;
+}
+
+int* carres(const int tab[]) {
+   const size_t N = sizeof(tab) / sizeof(int);
+   int copie[N];
+
+   for(size_t i = 0; i < N; ++i)
+      copie[i] = tab[i] * tab[i];
+
+   return copie;
 }

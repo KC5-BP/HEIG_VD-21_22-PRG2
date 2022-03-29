7d9e62c1e984542982074e2057905efab1f40672
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-29 19:13:06 +0200

    1.11 Cleans

--- corr/Chap1-Intro/1.11-printf3.c
+++ corr/Chap1-Intro/1.11-printf3.c
@@ -1,37 +1,55 @@
-// Exo 1.11 : Donnee ;
-//                  Completer le code de telle maniere, qu'il affiche :
-//                  i = 1
-//                  j = 4294967295
 /*
-#include <stdio.h>
-#include <stdlib.h>
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 1.11-printf3.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
 
-<a completer>
+ Description    : 1.11
+                     Completer le code de telle maniere, qu'il affiche :
+                        i = 1
+                        j = 4294967295
 
-int main(void) {
-    size_t i = 1;
-    uint32_t j = UINT32_MAX;
+ Remarque(s)    : <� compl�ter>
 
-    <a completer>
-
-    return EXIT_SUCCESS;
-}
- */
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
 
-#define PRINT_VAR(VAR) (		   \
-	printf(#VAR " = %lu\n", VAR)	\
+#define PRINT_VAR(VAR) (                              \
+    printf(#VAR " = %" PRIu64 "\n", (uint64_t) VAR)   \
 )
 
 int main(void) {
 	size_t i = 1;
 	uint32_t j = UINT32_MAX;
 
-	PRINT_VAR(i);
-	PRINT_VAR(j);
+   // Si gcc 32 bits
+   //printf("i = %u\n", i);
+   //printf("i = %" PRIu32 "\n", i);
+   //printf("i = %" PRIu64 "\n", (uint64_t) i);
+   //printf("i = %" PRIuMAX "\n", (uintmax_t) i);
+
+   // Si gcc 64 bits
+   //printf("i = %" PRIu64 "\n", i);
+   //printf("i = %" PRIuMAX "\n", i);
+   //printf("i = %" PRIu64 "\n", (uint64_t) i);
+   //printf("i = %" PRIuMAX "\n", (uint64_t) i);
+
+   //printf("j = %u\n", j);
+   //printf("j = %d\n", j);  // pas interdit mais ...
+   //printf("j = %" PRIu8 "\n", j);
+   //printf("j = %" PRIu16 "\n", j);
+   //printf("j = %" PRIu32 "\n", j);  // mais pas PRIu64, ni PRIuMAX (warning!)
+   //printf("j = %" PRIu64 "\n", (uintmax_t) j);
+   //printf("j = %" PRIuMAX "\n", (uint64_t) j);
+
+   // Using a macros having the good format for both value
+   PRINT_VAR(i);
+   PRINT_VAR(j);
 
 	return EXIT_SUCCESS;
 }

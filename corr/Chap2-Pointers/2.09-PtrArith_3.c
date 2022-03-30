f91644ccc3ac36a9c876a506cf203e1490b6e2bf
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-03-30 11:37:05 +0200

    2.09 Clean (just check for the numerical promotion for i) & j) )

--- corr/Chap2-Pointers/2.09-PtrArith_3.c
+++ corr/Chap2-Pointers/2.09-PtrArith_3.c
@@ -1,12 +1,25 @@
 /*
  -----------------------------------------------------------------------------------
- Nom du fichier : <nom du fichier>.<x> (x = h ou c)
- Auteur(s)      : K. BOUGNON-PEIGNE
+ Nom du fichier : 2.09-PtrArith_3.c
+ Auteur(s)      : (\_/)
  Date creation  : <jj.mm.aaaa>
 
- Description    : <� compl�ter>
+ Description    : 2.09
+                     Soit les declarations suivantes :
+                        a) c[3][0]
+                        b) (**d)[5]
+                        c) (**e)[*d-c]
+                        d) (d[3] - 3)[0][3]
+                        e) **d + 5
+                        f) *d[3] + 2
+                        g) *(*e[-3] + 5)
+                        h) **c
+                        i) e[0][0][e-d] + 1
+                        j) 0[c][0] - 'd' + 'B'
+                     Selon les declarations du code ..
 
- Remarque(s)    : <� compl�ter>
+ Remarque(s)    : Results of i & j should be in integer because of the numerical
+                  promotion while operating with
 
  Compilateur    : Mingw-w64 gcc 11.2.0
  -----------------------------------------------------------------------------------
@@ -16,6 +29,65 @@
 #include <stdlib.h>
 
 int main(void) {
-	// <code>
-	return EXIT_SUCCESS;
+   const char* c[] = {"elle", "mangera", "une", "petite", "tomme"};
+   const char** d[] = {c+1, c+2, c+3, c+4, c};
+   const char*** e = &d[3];
+
+   printf("a) %c\n", c[3][0]);   // a) 'p'
+   printf("b) %c\n", (**d)[5]);  // b) 'r'
+   printf("c) %c\n", (**e)[*d-c]);  // c) 'o'
+                                    // *e => c + 4
+                                    // **e => "tomme"
+                                    //
+                                    // *d => c+1
+                                    // *d - c => 1
+                                    // "tomme"[1] => 'o'
+   printf("d) %c\n", (d[3] - 3)[0][3]);   // d) 'g'
+                                          // (d[3]) - 3 => (c + 4) - 3 => c + 1
+                                          //
+                                          // [0] => "mangera"
+                                          // [0][3] => 'g'
+   printf("e) %s\n", **d + 5);   // e) "ra"
+                                 // *d => c + 1
+                                 // **d => "mangera"
+                                 // **d + 5 => "ra"
+   printf("f) %s\n", *d[3] + 2); // f) "mm"
+                                 // d[3] => c + 4
+                                 // *d[3] => "tomme"
+                                 // *d[3] + 2 => "mme"
+   printf("g) %c\n", *(*e[-3] + 5));   // g) 'r'
+                                       // e[-3] => d[0] => c + 1
+                                       // *e[-3] => "mangera"
+                                       // *e[-3] + 5 => "ra"
+                                       // *(*e[-3] + 5) => 'r'
+   printf("h) %c\n", **c); // h) 'e'
+                           // *c => "elle"
+                           // **c => 'e'
+   printf("i) %d (or %c as char)\n", e[0][0][e-d] + 1, e[0][0][e-d] + 1);
+                                                // i) valeur ASCII de 'n'
+                                                // e[0] => d[3] => c + 4
+                                                // e[0][0] => "tomme"
+                                                //
+                                                // [e-d] = 3
+                                                // e[0][0][e-d] => 'm'
+                                                // 'm' + 1 => valeur ASCII de 'n'
+   printf("j) %d (or %c as char)\n\n", 0[c][0] - 'd' + 'B', 0[c][0] - 'd' + 'B');
+                                 // j) valeur ASCII de 'C'
+                                 // 0[c][0] <=> c[0][0]
+                                 // 0[c][0] => 'e'
+                                 // 'e' - 'd' => 1
+                                 // 1 + 'B' => valeur ASCII de 'C'
+
+   printf("Message is : %c%c%c%c%s%s%c%c%c%c\n",   c[3][0],
+                                                   (**d)[5],
+                                                   (**e)[*d-c],
+                                                   (d[3] - 3)[0][3],
+                                                   **d + 5,
+                                                   *d[3] + 2,
+                                                   *(*e[-3] + 5),
+                                                   **c,
+                                                   e[0][0][e-d] + 1,
+                                                   0[c][0] - 'd' + 'B');
+
+   return EXIT_SUCCESS;
 }
--- template/Chap2-Pointers/2.09-PtrArith_3.c
+++ template/Chap2-Pointers/2.09-PtrArith_3.c
@@ -1,10 +1,22 @@
 /*
  -----------------------------------------------------------------------------------
- Nom du fichier : <nom du fichier>.<x> (x = h ou c)
- Auteur(s)      : K. BOUGNON-PEIGNE
+ Nom du fichier : 2.09-PtrArith_3.c
+ Auteur(s)      : (\_/)
  Date creation  : <jj.mm.aaaa>
 
- Description    : <� compl�ter>
+ Description    : 2.09
+                     Soit les declarations suivantes :
+                        a) c[3][0]
+                        b) (**d)[5]
+                        c) (**e)[*d-c]
+                        d) (d[3] - 3)[0][3]
+                        e) **d + 5
+                        f) *d[3] + 2
+                        g) *(*e[-3] + 5)
+                        h) **c
+                        i) e[0][0][e-d] + 1
+                        j) 0[c][0] - 'd' + 'B'
+                     Selon les declarations du code ..
 
  Remarque(s)    : <� compl�ter>
 
@@ -16,6 +28,29 @@
 #include <stdlib.h>
 
 int main(void) {
-	// <code>
+   const char* c[] = {"elle", "mangera", "une", "petite", "tomme"};
+   const char** d[] = {c+1, c+2, c+3, c+4, c};
+   const char*** e = &d[3];
+
+   printf("a) %\n", c[3][0]);
+
+   printf("b) %\n", (**d)[5]);
+
+   printf("c) %\n", (**e)[*d-c]);
+
+   printf("d) %\n", (d[3] - 3)[0][3]);
+
+   printf("e) %\n", **d + 5);
+
+   printf("f) %\n", *d[3] + 2);
+
+   printf("g) %\n", *(*e[-3] + 5));
+
+   printf("h) %\n", **c);
+
+   printf("i) %\n", e[0][0][e-d] + 1);
+
+   printf("j) %\n", 0[c][0] - 'd' + 'B');
+
 	return EXIT_SUCCESS;
 }

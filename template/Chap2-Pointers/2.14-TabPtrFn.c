df21c6ba8d907d2146d64564a01fb4e947e1948c
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-01 18:41:11 +0200

    2.14 Clean

---
+++ template/Chap2-Pointers/2.14-TabPtrFn.c
@@ -0,0 +1,45 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 2.14-TabPtrFn.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 2.14
+                     Que va afficher le code suivant ?
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void f(int* p1, int p2);
+
+int main(void) {
+   int a = 5, b = 6;
+   int t[2] = {3, 4};
+   int *p = NULL, *q = NULL;
+
+   f(&a, b);
+   printf("1) %d, %d\n", a, b);
+
+   p = &a;
+   q = &b;
+   f(q, *p);
+   printf("2) %d, %d\n", *p, *q);
+
+   f(t + 1, *t);
+   printf("3) %d, %d\n", t[0], t[1]);
+
+   f((int*)(&p), b);
+   printf("4) %d, %d\n", a, b);
+
+   return EXIT_SUCCESS;
+}
+
+void f(int* p1, int p2) {
+   *p1 = 2 * p2;
+}

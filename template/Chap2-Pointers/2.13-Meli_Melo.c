564ac29140c9d9e1add2075d9674d3bade6f392e
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-01 18:37:18 +0200

    2.13 Added

---
+++ template/Chap2-Pointers/2.13-Meli_Melo.c
@@ -0,0 +1,34 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 2.13-Meli_Melo.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 2.13
+                     Que va afficher le code suivant (
+
+                     Remarque : Suppose que la machine est d'une architecture 32bits
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+   int i = 1, j = 2;
+   int *p = &i, *q = &j;
+
+   *p = (int) q;
+   *q = (int) p;
+   p = (int*) *p;
+   q = (int*) *q;
+
+   i = 3, j = 4;
+   printf("*p = %d *q = %d\n", *p, *q);
+
+   return EXIT_SUCCESS;
+}

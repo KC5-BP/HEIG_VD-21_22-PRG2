860ac7f2809739f9b126cf97af172ae4ca096ba4
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-19 21:54:59 +0200

    1.02-Macros_1 : Correct the code to make it display $ 25 -1

--- corr/Chap1-Intro/1.02-Macros1.c
+++ corr/Chap1-Intro/1.02-Macros1.c
@@ -19,10 +19,13 @@
 
 #define A 2
 #define B (A + 1)
-#define PLUS(X,Y) ( (X)+(Y) )
-#define MOINS(X,Y) ( (X)-(Y) )
+#define PLUS(X, Y) ( (X)+(Y) )
+#define MOINS(X, Y) ( (X)-(Y) )
 
 int main() {
-	printf("%d %d\n", 5*PLUS(A,B), MOINS(A+1,B+1));
-	return EXIT_SUCCESS;
+    printf("%d %d\n", 5 * PLUS(A, B), MOINS(A + 1, B + 1));
+    return EXIT_SUCCESS;
 }
+
+// Output :
+// 25 -1
--- template/Chap1-Intro/1.02-Macros1.c
+++ template/Chap1-Intro/1.02-Macros1.c
@@ -20,9 +20,9 @@
 #define A 2
 #define B = A + 1;
 #define PLUS (X,Y) X+Y;
-#define MOINS(X,Y) X-Y;
+#define MOINS(X, Y) X-Y;
 
 int main() {
-   printf("%d %d\n", 5*PLUS(A,B), MOINS(A+1,B+1));
-   return EXIT_SUCCESS;
+    printf("%d %d\n", 5 * PLUS(A, B), MOINS(A + 1, B + 1));
+    return EXIT_SUCCESS;
 }

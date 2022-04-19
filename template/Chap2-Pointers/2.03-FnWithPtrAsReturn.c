673daf9c9437e9c3b92147339ddb7ae612c04ab0
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-20 00:30:26 +0200

    Function with ptr as return value : What will be done at execution?

--- template/Chap2-Pointers/2.03-FnWithPtrAsReturn.c
+++ template/Chap2-Pointers/2.03-FnWithPtrAsReturn.c
@@ -16,26 +16,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* carres(const int tab[]);
+int *carres(const int tab[]);
 
 int main() {
-   int tab[] = {1, 2, 3};
-   const size_t N = sizeof(tab) / sizeof(int);
-   int* ptr = carres(tab);
+    int tab[] = {1, 2, 3};
+    const size_t N = sizeof(tab) / sizeof(int);
+    int *ptr = carres(tab);
 
-   for(size_t i = 0; i < N; ++i)
-      printf("%d ", ptr[i]);
+    for (size_t i = 0; i < N; ++i)
+        printf("%d ", ptr[i]);
 
-   printf("\n");
-   return EXIT_SUCCESS;
+    printf("\n");
+    return EXIT_SUCCESS;
 }
 
-int* carres(const int tab[]) {
-   const size_t N = sizeof(tab) / sizeof(int);
-   int copie[N];
+int *carres(const int tab[]) {
+    const size_t N = sizeof(tab) / sizeof(int);
+    int copie[N];
 
-   for(size_t i = 0; i < N; ++i)
-      copie[i] = tab[i] * tab[i];
+    for (size_t i = 0; i < N; ++i)
+        copie[i] = tab[i] * tab[i];
 
-   return copie;
+    return copie;
 }

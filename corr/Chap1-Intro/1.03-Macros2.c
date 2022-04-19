2c5a9fdcde996cb216ed4484724777fed00d4c88
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-19 22:20:49 +0200

    1.03-Macros_2 : 1) What will it display? 2) Explain the problem(s). 3) Which way to avoid it/them?

--- corr/Chap1-Intro/1.03-Macros2.c
+++ corr/Chap1-Intro/1.03-Macros2.c
@@ -11,8 +11,8 @@
                         fonction abs de stdlib) la macro ABS de maniere
                         a eviter les problemes du pt2 ?
 
- Remarque(s)    : 1) See code below
-                  2) ° Change the waz of working if not -(X) like seen with 4
+ Remarque(s)    : 1) See "Output" after each main
+                  2) ° Change the way of working if not -(X) like seen with 4
                      ° And multiple increase instead of a simple one
                         ("Side effect" / "Effet de bord" on 5 & 6) .
                   3) ° Yes, by creating a "generic" macros for an abs fn.
@@ -27,106 +27,97 @@
 // Uncomment to see the correct output after pt3 has been done.
 //#define PT3_CORRECTED 0
 #ifndef PT3_CORRECTED
+    // Uncomment to see the correct output after pt2 has been done.
+    //#define PT2_CORRECTED 0
+    #ifndef PT2_CORRECTED
+        #define ABS(X) (((X) > 0) ? (X) : (-X))
+    #else
+        #define ABS(X) (((X) > 0) ? (X) : -(X))
+    #endif  // END PT2_CORRECTED
 
-      #define PT2_CORRECTED 0
-      #ifndef PT2_CORRECTED
-         #define ABS(X) (((X) > 0) ? (X) : (-X))
-      #else
-         #define ABS(X) (((X) > 0) ? (X) : -(X))
-      #endif   // END PT2_CORRECTED
+    int main(void) {
+        int n;
 
-   int main(void) {
-      int n;
+        printf("1. %d\n", ABS(2));
+        printf("2. %d\n", ABS(-2));
+        printf("3. %d\n", ABS('A'));
 
-      printf("1. %d\n", ABS(2));
-      // 1) Will print 2
-      printf("2. %d\n", ABS(-2));
-      // 2) Will print 2
-      printf("3. %d\n", ABS('A'));
-      // 3) Will print 65
+        n = -2;
+        printf("4. %d\n", ABS(n + 1));
+        // BEFORE PT2 : ( ((n+1) > 0) ? (n+1) : (-n+1) )
+        // AFTER PT2  : ( ((n+1) > 0) ? (n+1) : (-(n+1)) )
 
-      n = -2;
-      printf("4. %d\n", ABS(n+1));
-      // (((n+1) > 0) ? (n+1) : (-n+1))
-      // 4) Will print 3
+        n = -2;
+        printf("5. %d", ABS(n++));
+        printf(" %d\n", n);
+        // (((n++) > 0) ? (n++) : (-n++))
+        // After test : -1
+        //      Will print 1 for first %d
+        //          Will print 0 for second %d because of post increase before.
 
-      n = -2;
-      printf("5. %d", ABS(n++)); printf(" %d\n", n);
-      // (((n++) > 0) ? (n++) : (-n++))
-      // After test : -1
-      //      Will print 1 for first %d
-      //          Will print 0 for second %d because of post increase before.
-      // 5) 1 0
-
-      n = -2;
-      printf("6. %d", ABS(++n)); printf(" %d\n", n);
-      // (((++n) > 0) ? (++n) : (-++n))
-      // After test : -1
-      //      Will print 0 for first %d because pre increase and negate after.
-      //          Will print 0 for second %d.
-      // 6) 0 0
+        n = -2;
+        printf("6. %d", ABS(++n));
+        printf(" %d\n", n);
+        // (((++n) > 0) ? (++n) : (-++n))
+        // After test : -1
+        //      Will print 0 for first %d because pre increase and negate after.
+        //          Will print 0 for second %d.
 
 
-      n = -2;
-      printf("7. %d", abs(++n)); printf(" %d\n", n);
-      // 7) Will print 1 -1 by using abs from stdlib
+        n = -2;
+        printf("7. %d", abs(++n));
+        printf(" %d\n", n);
 
-      return EXIT_SUCCESS;
-   }
+        return EXIT_SUCCESS;
+    }
+    // Output :
+    //1. 2      -> AFTER PT2 : 1. 2
+    //2. 2      -> AFTER PT2 : 2. 2
+    //3. 65     -> AFTER PT2 : 3. 65
+    //4. 3      -> AFTER PT2 : 4. 1
+    //5. 1 0    -> AFTER PT2 : 5. 1 0
+    //6. 0 0    -> AFTER PT2 : 6. 0 0
+    //7. 1 -1   -> AFTER PT2 : 7. 1 -1
 
 #else // PT3 defined
+    #define GENERIC_ABS(type)                   \
+        type abs_##type(type input) {           \
+            return input < 0 ? -input : input;  \
+        }
 
-   #define GENERIC_ABS(type)					\
-   type abs_##type(type input) {				\
-      return input < 0 ? -input : input;	\
-   }
-   GENERIC_ABS(int)
+    GENERIC_ABS(int)
 
-   int main(void) {
-      int n;
+    int main(void) {
+        int n;
 
-      printf("1. %d\n", abs_int(2));
-      // 1) Will print 2
-      printf("2. %d\n", abs_int(-2));
-      // 2) Will print 2
-      printf("3. %d\n", abs_int('A'));
-      // 3) Will print 65
+        printf("1. %d\n", abs_int(2));
+        printf("2. %d\n", abs_int(-2));
+        printf("3. %d\n", abs_int('A'));
 
-      n = -2;
-      printf("4. %d\n", abs_int(n+1));
-      // 4) Will print 1
+        n = -2;
+        printf("4. %d\n", abs_int(n + 1));
 
-      n = -2;
-      printf("5. %d", abs_int(n++)); printf(" %d\n", n);
-      // 5) 2 -1
+        n = -2;
+        printf("5. %d", abs_int(n++));
+        printf(" %d\n", n);
 
-      n = -2;
-      printf("6. %d", abs_int(++n)); printf(" %d\n", n);
-      // 6) 1 -1
+        n = -2;
+        printf("6. %d", abs_int(++n));
+        printf(" %d\n", n);
 
-      n = -2;
-      printf("7. %d", abs(++n)); printf(" %d\n", n);
-      // 7) 1 -1
+        n = -2;
+        printf("7. %d", abs(++n));
+        printf(" %d\n", n);
 
-      return EXIT_SUCCESS;
-   }
-#endif   // END PT3_CORRECTED
+        return EXIT_SUCCESS;
+    }
+    // Output :
+    //1. 2
+    //2. 2
+    //3. 65
+    //4. 1
+    //5. 2 -1
+    //6. 1 -1
+    //7. 1 -1
 
-/*
- * Side note(s) :
-#define GENERIC_ABS(type)					\
-type abs_##type(type input) {				\
-	return input > 0 ? input : -input;	\
-}
- * And used as :
-GENERIC_ABS(int) // Creating the fn : int abs_int(int input) { ... }
-
- * Note(s) :	abs_##type() OR type##_abs()
-							'-------------'---> '#' Concatenate the value as a name
-														instead of copying it like a simple define do.
- * 				We can use the '#' into a define to print a var name :
-#define VIEW_VAR(a) printf(#a " = %d", (a))
- * 				That will print : <varName> = %d
- * 				We call that : Stringizing in english / Stringification en francais.
- * 				Usage only possible in macros.
- */
+#endif  // END PT3_CORRECTED
--- template/Chap1-Intro/1.03-Macros2.c
+++ template/Chap1-Intro/1.03-Macros2.c
@@ -23,23 +23,23 @@
 #define ABS(X) (((X) > 0) ? (X) : (-X))
 
 int main(void) {
-   int n;
+    int n;
 
-   printf("1. %d\n", ABS(2));
-   printf("2. %d\n", ABS(-2));
-   printf("3. %d\n", ABS('A'));
+    printf("1. %d\n", ABS(2));
+    printf("2. %d\n", ABS(-2));
+    printf("3. %d\n", ABS('A'));
 
-   n = -2;
-   printf("4. %d\n", ABS(n+1));
+    n = -2;
+    printf("4. %d\n", ABS(n + 1));
 
-   n = -2;
-   printf("5. %d", ABS(n++)); printf(" %d\n", n);
+    n = -2;
+    printf("5. %d", ABS(n++));      printf(" %d\n", n);
 
-   n = -2;
-   printf("6. %d", ABS(++n)); printf(" %d\n", n);
+    n = -2;
+    printf("6. %d", ABS(++n));      printf(" %d\n", n);
 
-   n = -2;
-   printf("7. %d", abs(++n)); printf(" %d\n", n);
+    n = -2;
+    printf("7. %d", abs(++n));      printf(" %d\n", n);
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }

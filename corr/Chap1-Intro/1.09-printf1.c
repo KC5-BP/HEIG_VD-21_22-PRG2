e1d8d42d06f49a44a9598fdce7e0bbe70ec8544c
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-19 23:21:34 +0200

    1.09-printf_1 : What will it display?

--- corr/Chap1-Intro/1.09-printf1.c
+++ corr/Chap1-Intro/1.09-printf1.c
@@ -17,17 +17,27 @@
 #include <stdlib.h>
 
 int main(void) {
-	int n = 127;
-	double x = 12.3456;
+    int n = 127;
+    double x = 12.3456;
 
-	printf("R1 : |%-4d|\n", n);   // |127 |
-	printf("R2 : |%04d|\n", n);   // |0127|
-	printf("R3 : |%x|\n", n);     // |7f|
-	printf("R4 : |%#o|\n", n);    // |0177|
-	printf("R5 : |%f|\n", x);     // |12.345600|
-	printf("R6 : |%5.2f|\n", x);  // |12.35|
-	printf("R7 : |%.2e|\n", x);   // |1.23e+01|
-	printf("R8 : |%g|\n", x);     // |12.3456|
+    printf("R1 : |%-4d|\n", n);
+    printf("R2 : |%04d|\n", n);
+    printf("R3 : |%x|\n", n);
+    printf("R4 : |%#o|\n", n);
+    printf("R5 : |%f|\n", x);
+    printf("R6 : |%5.2f|\n", x);
+    printf("R7 : |%.2e|\n", x);
+    printf("R8 : |%g|\n", x);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
+
+// Output :
+//R1 : |127 |
+//R2 : |0127|
+//R3 : |7f|
+//R4 : |0177|
+//R5 : |12.345600|
+//R6 : |12.35|
+//R7 : |1.23e+01|
+//R8 : |12.3456|
--- template/Chap1-Intro/1.09-printf1.c
+++ template/Chap1-Intro/1.09-printf1.c
@@ -17,17 +17,17 @@
 #include <stdlib.h>
 
 int main(void) {
-	int n = 127;
-	double x = 12.3456;
+    int n = 127;
+    double x = 12.3456;
 
-	printf("R1 : |%-4d|\n", n);
-	printf("R2 : |%04d|\n", n);
-	printf("R3 : |%x|\n", n);
-	printf("R4 : |%#o|\n", n);
-	printf("R5 : |%f|\n", x);
-	printf("R6 : |%5.2f|\n", x);
-	printf("R7 : |%.2e|\n", x);
-	printf("R8 : |%g|\n", x);
+    printf("R1 : |%-4d|\n", n);
+    printf("R2 : |%04d|\n", n);
+    printf("R3 : |%x|\n", n);
+    printf("R4 : |%#o|\n", n);
+    printf("R5 : |%f|\n", x);
+    printf("R6 : |%5.2f|\n", x);
+    printf("R7 : |%.2e|\n", x);
+    printf("R8 : |%g|\n", x);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }

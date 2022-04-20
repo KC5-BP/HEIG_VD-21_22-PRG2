67150d54dff009ca30c3c2ae9c4bf7434651f9c7
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-20 11:12:35 +0200

    2.07-Pointer's Arithmetic : What will it display?

--- corr/Chap2-Pointers/2.07-PtrArith_1.c
+++ corr/Chap2-Pointers/2.07-PtrArith_1.c
@@ -7,7 +7,21 @@
  Description    : 2.07
                      Que va afficher le main ci-dessous ?
 
- Remarque(s)    : <� compl�ter>
+ Remarque(s)    : Memory view
+                  (or simply look into Corr-2.06_to_2.09_2.12_2.14.pdf)
+                    .----. .----. .----. .----. .----.
+                a : | 10 | | 20 | | 30 | | 40 | | 50 |
+                    '----' '----' '----' '----' '----'
+                       ^      ^      ^      ^      ^
+                    .--|-. .--|-. .--|-. .--|-. .--|-.
+                p : |  ° | |  ° | |  ° | |  ° | |  ° |
+                    '----' '----' '----' '----' '----'
+                                     ^
+                    .----.        .--|-.
+                    |  ° |  --->  |  ° |
+                    '----'        '----'
+                     ppp            pp
+
 
  Compilateur    : Mingw-w64 gcc 11.2.0
  -----------------------------------------------------------------------------------
@@ -19,41 +33,50 @@
 #include <stdio.h>
 
 int main(void) {
-   int a[] = {10, 20, 30, 40, 50};
-   int* p[] = {a, a+1, a+2, a+3, a+4};
-   int** pp = &p[2];
-   int*** ppp = &pp;
+    int a[] = {10, 20, 30, 40, 50};
+    int* p[] = {a, a + 1, a + 2, a + 3, a + 4};
+    int** pp = &p[2];
+    int*** ppp = &pp;
 
-   printf("1) %d\n", *a);     // 1) 10
-   printf("2) %d\n", **p);    // 2) 10
-   printf("3) %d\n", **pp++); // 3) 30
-                              // But now, pp point on &p[3]
-   printf("4) %d\n", ***ppp); // 4) 40
-   pp = p;
-   printf("5) %d\n", **++pp); // 5) 20
-                              // pp => &p[0]
-                              // ++pp => &p[1]
-                              // *++pp => a + 1
-                              // **++pp => 20
-   pp = p;
-   printf("6) %d\n", ++**pp); // 6) 11
-                              // pp => &p[0]
-                              // *pp => a
-                              // **pp => 10
-                              // ++**pp => 11
-   pp = p;
-   printf("7) %d\n", *pp[1]); // 7) 20
-                              // pp => &p[0]
-                              // pp[1] => a + 1
-                              // *pp[1] => 20
-   pp = p;
-   printf("8) %" PRIdPTR "\n", (ptrdiff_t) (*(p+1) - *pp)); // 8) 1
-                                                            // *(p+1) <=> p[1]
-                                                            //             => a + 1
-                                                            //
-                                                            // pp => &p[0]
-                                                            // *pp => a
-                                                            // a + 1 - a = 1
+    printf("1) %d\n", *a);
+    printf("2) %d\n", **p);
+    printf("3) %d\n", **pp++);  // After that, pp point on &p[3]
+    printf("4) %d\n", ***ppp);
+    pp = p;
+    printf("5) %d\n", **++pp);
+                                // pp => &p[0]
+                                // ++pp => &p[1]
+                                // *++pp => a + 1
+                                // **++pp => 20
+    pp = p;
+    printf("6) %d\n", ++**pp);
+                                // pp => &p[0]
+                                // *pp => a
+                                // **pp => 10
+                                // ++**pp => 11
+    pp = p;
+    printf("7) %d\n", *pp[1]);
+                                // pp => &p[0]
+                                // pp[1] => a + 1
+                                // *pp[1] => 20
+    pp = p;
+    printf("8) %" PRIdPTR "\n", (ptrdiff_t) (*(p + 1) - *pp));
+                                // *(p+1) <=> p[1] => a + 1
+                                //
+                                // pp => &p[0]
+                                // *pp => a
+                                //
+                                // a + 1 - a = 1
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
+
+// Output :
+//1) 10
+//2) 10
+//3) 30
+//4) 40
+//5) 20
+//6) 11
+//7) 20
+//8) 1
--- template/Chap2-Pointers/2.07-PtrArith_1.c
+++ template/Chap2-Pointers/2.07-PtrArith_1.c
@@ -19,23 +19,23 @@
 #include <stdio.h>
 
 int main(void) {
-   int a[] = {10, 20, 30, 40, 50};
-   int* p[] = {a, a+1, a+2, a+3, a+4};
-   int** pp = &p[2];
-   int*** ppp = &pp;
+    int a[] = {10, 20, 30, 40, 50};
+    int* p[] = {a, a + 1, a + 2, a + 3, a + 4};
+    int** pp = &p[2];
+    int*** ppp = &pp;
 
-   printf("1) %d\n", *a);
-   printf("2) %d\n", **p);
-   printf("3) %d\n", **pp++);
-   printf("4) %d\n", ***ppp);
-   pp = p;
-   printf("5) %d\n", **++pp);
-   pp = p;
-   printf("6) %d\n", ++**pp);
-   pp = p;
-   printf("7) %d\n", *pp[1]);
-   pp = p;
-   printf("8) %" PRIdPTR "\n", (ptrdiff_t) (*(p+1) - *pp));
+    printf("1) %d\n", *a);
+    printf("2) %d\n", **p);
+    printf("3) %d\n", **pp++);
+    printf("4) %d\n", ***ppp);
+    pp = p;
+    printf("5) %d\n", **++pp);
+    pp = p;
+    printf("6) %d\n", ++**pp);
+    pp = p;
+    printf("7) %d\n", *pp[1]);
+    pp = p;
+    printf("8) %" PRIdPTR "\n", (ptrdiff_t) (*(p + 1) - *pp));
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }

82e0263ec052a785e47943ab7ab74295e8612111
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-20 20:19:50 +0200

    2.14-Pointers Array & Functions : What will it display?

--- corr/Chap2-Pointers/2.14-TabPtrFn.c
+++ corr/Chap2-Pointers/2.14-TabPtrFn.c
@@ -19,31 +19,32 @@
 void f(int* p1, int p2);
 
 int main(void) {
-   int a = 5, b = 6;
-   int t[2] = {3, 4};
-   int *p = NULL, *q = NULL;
+    int a = 5, b = 6;
+    int t[2] = {3, 4};
+    int* p = NULL, * q = NULL;
 
-   f(&a, b);
-   printf("1) %d, %d\n", a, b);
+    f(&a, b);
+    printf("1) %d, %d\n", a, b);
 
-   p = &a;
-   q = &b;
-   f(q, *p);
-   printf("2) %d, %d\n", *p, *q);
+    p = &a;
+    q = &b;
+    f(q, *p);
+    printf("2) %d, %d\n", *p, *q);
 
-   f(t + 1, *t);
-   printf("3) %d, %d\n", t[0], t[1]);
+    f(t + 1, *t);
+    printf("3) %d, %d\n", t[0], t[1]);
 
-   f((int*)(&p), b);
-   printf("4) %d, %d\n", a, b);
+    f((int*) (&p), b);
+    printf("4) %d, %d\n", a, b);
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 void f(int* p1, int p2) {
-   *p1 = 2 * p2;
+    *p1 = 2 * p2;
 }
-// Outputs
+
+// Output :
 //1) 12, 6
 //2) 12, 24
 //3) 3, 6

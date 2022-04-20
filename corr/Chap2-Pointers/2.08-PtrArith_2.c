712fc574c89348b97cae9d356d71eae821588fa4
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-20 11:19:29 +0200

    2.08-Pointer's Arithmetic 2 : What will it display?

--- corr/Chap2-Pointers/2.08-PtrArith_2.c
+++ corr/Chap2-Pointers/2.08-PtrArith_2.c
@@ -17,23 +17,24 @@
 #include <stdlib.h>
 
 int main(void) {
-   int t[6], i, *p;
+    int t[6], i, * p;
 
-   for (p = &t[5]; p > &t[-1]; p--) // Filling BACKWARD << t >>
-      *p = (int) (p - t);
+    for (p = &t[5]; p > &t[-1]; p--)    // Filling BACKWARD << t >>
+        *p = (int) (p - t);
 
-   for (i = 0; i < 6; i++)          // Print the content ONWARD of << t >>
-      printf("%d ", t[i]);
-   printf("\n");
+    for (i = 0; i < 6; i++)             // Print the content ONWARD of << t >>
+        printf("%d ", t[i]);
+    printf("\n");
 
-   for (i = 0, ++p; i < 6; i++)     // Print the integer part of each pointed value
-      printf("%d ", *p++/2);        // divided by 2 of << p >>
-   printf("\n");                    // Detail(s) : We went to p > &t[-1], so to
-                                    // start over from &t[0], p need a increase
-                                    // before starting the loop.
+    for (i = 0, ++p; i < 6; i++)        // Print the integer part of each pointed value
+        printf("%d ", *p++ / 2);        // divided by 2 of << p >>
+    printf("\n");                       // Detail(s) : We went to p > &t[-1], so to
+                                        // start over from &t[0], p need an increase
+                                        // before starting the loop => ++p; .
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
+
 // Output :
-// 0 1 2 3 4 5
-// 0 0 1 1 2 2
+//0 1 2 3 4 5
+//0 0 1 1 2 2
--- template/Chap2-Pointers/2.08-PtrArith_2.c
+++ template/Chap2-Pointers/2.08-PtrArith_2.c
@@ -17,18 +17,18 @@
 #include <stdlib.h>
 
 int main(void) {
-   int t[6], i, *p;
+    int t[6], i, * p;
 
-   for (p = &t[5]; p > &t[-1]; p--)
-      *p = (int) (p - t);
+    for (p = &t[5]; p > &t[-1]; p--)
+        *p = (int) (p - t);
 
-   for (i = 0; i < 6; i++)
-      printf("%d ", t[i]);
-   printf("\n");
+    for (i = 0; i < 6; i++)
+        printf("%d ", t[i]);
+    printf("\n");
 
-   for (i = 0, ++p; i < 6; i++)
-      printf("%d ", *p++/2);
-   printf("\n");
+    for (i = 0, ++p; i < 6; i++)
+        printf("%d ", *p++ / 2);
+    printf("\n");
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }

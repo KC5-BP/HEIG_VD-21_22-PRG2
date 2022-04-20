225b0e7440e9ac7023566c2a8dcc14fe01ea9219
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-04-20 21:06:01 +0200

    2.16-1D Array inverter 2

--- corr/Chap2-Pointers/2.16-InvertArray1D_2.c
+++ corr/Chap2-Pointers/2.16-InvertArray1D_2.c
@@ -29,73 +29,71 @@ void afficher(const int* tab, size_t taille);
 void test(const int* tab, size_t taille);
 
 int main(void) {
-   {
-      const int TAB[] = {1};
-      test(TAB, sizeof(TAB) / sizeof(int));
-   }
-   {
-      const int TAB[] = {1, 2};
-      test(TAB, sizeof(TAB) / sizeof(int));
-   }
-   {
-      const int TAB[] = {1, 2, 3};
-      test(TAB, sizeof(TAB) / sizeof(int));
-   }
-   {
-      const int TAB[] = {1, 2, 3};
-      test(TAB, 0);
-   }
-	return EXIT_SUCCESS;
+    {
+        const int TAB[] = {1};
+        test(TAB, sizeof(TAB) / sizeof(int));
+    }
+    {
+        const int TAB[] = {1, 2};
+        test(TAB, sizeof(TAB) / sizeof(int));
+    }
+    {
+        const int TAB[] = {1, 2, 3};
+        test(TAB, sizeof(TAB) / sizeof(int));
+    }
+    {
+        const int TAB[] = {1, 2, 3};
+        test(TAB, 0);
+    }
+    return EXIT_SUCCESS;
 }
 
 int* inverser(const int* debut, const int* fin) {
-   assert(debut != NULL);
-   assert(fin != NULL);
-   assert(fin == debut - 1 || debut <= fin);
-   const size_t TAILLE = (size_t) (fin - debut + 1);
-   int* ptr = (int*) calloc(TAILLE, sizeof(int));
-   if (ptr)
-      for (size_t i = 0; i < TAILLE; ++i)
-         ptr[TAILLE - 1 - i] = debut[i];  // OR ptr[i] = *fin--;
-   return ptr;
+    assert(debut != NULL);
+    assert(fin != NULL);
+    //assert(fin == debut - 1 || debut <= fin);
+    assert(fin - debut + 1 > 0);
+    const size_t TAILLE = (size_t) (fin - debut + 1);
+    int* ptr = (int*) calloc(TAILLE, sizeof(int));
+    if (ptr)
+        for (size_t i = 0; i < TAILLE; ++i)
+            ptr[TAILLE - 1 - i] = debut[i];  // OR ptr[i] = *fin--;
+    return ptr;
 }
 
 void afficher(const int* tab, size_t taille) {
-   assert(tab != NULL);
-   printf("[");
-   for (size_t i = 0; i < taille; ++i) {
-      if (i > 0)
-         printf("%s", ", ");
-      printf("%d", tab[i]);
-   }
-   printf("]\n");
+    assert(tab != NULL);
+    printf("[");
+    for (size_t i = 0; i < taille; ++i) {
+        if (i > 0)
+            printf("%s", ", ");
+        printf("%d", tab[i]);
+    }
+    printf("]\n");
 }
 
 void test(const int* tab, size_t taille) {
-   printf("Avant inversion :\n");
-   afficher(tab, taille);
-   int* ptr = inverser(tab, tab + taille - 1);
-   printf("Apres inversion :\n");
-   afficher(ptr, taille);
-   printf("\n");
-   free(ptr);
+    printf("Avant inversion :\n");
+    afficher(tab, taille);
+    int* ptr = inverser(tab, tab + taille - 1);
+    printf("Apres inversion :\n");
+    afficher(ptr, taille);
+    free(ptr);
 }
+
 // Output :
 //Avant inversion :
 //[1]
 //Apres inversion :
 //[1]
-//
 //Avant inversion :
 //[1, 2]
 //Apres inversion :
 //[2, 1]
-//
 //Avant inversion :
 //[1, 2, 3]
 //Apres inversion :
 //[3, 2, 1]
-//
 //Avant inversion :
 //[]
 //Apres inversion :

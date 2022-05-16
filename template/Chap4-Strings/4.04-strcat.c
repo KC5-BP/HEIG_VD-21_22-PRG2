4e3b118f2924049df9572b9c7d09ff6f3d56d30f
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-16 18:47:14 +0200

    4.04-strcat

---
+++ template/Chap4-Strings/4.04-strcat.c
@@ -0,0 +1,37 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 4.04-strcat.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 4.04
+                    Proposez une implémentation de la fonction strcat dont le
+                    prototype et la sémantique sont donnés ci-dessous :
+char* strcat(char* to, const char* from);
+
+                    Semantique :
+                        Concatène la chaîne from à la suite de la chaîne to et
+                        retourne un pointeur sur to.
+
+                    Ecrire aussi un petit programme permettant de tester votre
+                    fonction.
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* strcat(char* to, const char* from);
+
+int main(void) {
+	// <code>
+	return EXIT_SUCCESS;
+}
+
+char* strcat(char* to, const char* from) {
+
+}

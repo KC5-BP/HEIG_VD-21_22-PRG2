334dcb79b2f502c3978a283e2dca648f96503c65
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-16 18:52:46 +0200

    4.09-String inversion : Write a function that allows the user to invert a string from 2 different ways.

---
+++ template/Chap4-Strings/4.09-StrInverter.c
@@ -0,0 +1,47 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 4.09-StrInverter.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 4.09
+                    Proposer une implémentation pour chacune des fonctions
+                    d'inversion d'une chaîne suivantes :
+                        1. void  inverser_1(char* s);
+                        2. char* inverser_2(const char* s);
+
+                        Nota Bene :
+                            Renvoie s en cas de problème
+
+                    Ecrire aussi un petit programme permettant de tester les
+                    deux fonctions. Lister les avantages/inconvénients respectifs
+                    des 2 fonctions ci-dessus.
+
+                    Nota Bene :
+                        Hormis strlen, aucune autre fonction de string.h
+                        ne doit être utilisée ici.
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void inverser_1(char* s);
+char* inverser_2(const char* s);
+
+int main(void) {
+	// <code>
+	return EXIT_SUCCESS;
+}
+
+void inverser_1(char* s) {
+
+}
+
+char* inverser_2(const char* s) {
+
+}

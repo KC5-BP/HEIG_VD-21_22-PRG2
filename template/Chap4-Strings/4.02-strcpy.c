611bef1ce3a73fdd5e117fc3659a02e510f607ad
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-16 18:46:18 +0200

    4.02-strcpy

---
+++ template/Chap4-Strings/4.02-strcpy.c
@@ -0,0 +1,37 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 4.02-strcpy.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 4.02
+                    Proposez une implémentation de la fonction strcpy dont le
+                    prototype et la sémantique sont donnés ci-dessous :
+char* strcpy(char* to, const char* from);
+
+                    Semantique :
+                        Recopie la chaîne from dans la chaîne to et
+                        retourne un pointeur sur to.
+
+                    Ecrire aussi un petit programme permettant de tester votre
+                    fonction.
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* strcpy(char* to, const char* from);
+
+int main(void) {
+	// <code>
+	return EXIT_SUCCESS;
+}
+
+char* strcpy(char* to, const char* from) {
+
+}

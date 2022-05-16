b38fc31e6e15a999d3aef7a6dcaad0c6e539f87b
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-16 18:47:37 +0200

    4.06-strcmp

---
+++ template/Chap4-Strings/4.06-strcmp.c
@@ -0,0 +1,37 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 4.06-strcmp.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 4.06
+                    Proposez une implémentation de la fonction strcmp dont le
+                    prototype et la sémantique sont donnés ci-dessous :
+int strcmp(const char* s, const char* t);
+
+                    Semantique :
+                        Compare les deux chaîne s et t et retourne un résultat > 0
+                        si s > t, = 0 si s = t et < 0 si s < t.
+
+                    Ecrire aussi un petit programme permettant de tester votre
+                    fonction.
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int strcmp(const char* s, const char* t);
+
+int main(void) {
+	// <code>
+	return EXIT_SUCCESS;
+}
+
+int strcmp(const char* s, const char* t) {
+
+}

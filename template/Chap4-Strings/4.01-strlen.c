98527f9def3b569cb4cd216cad93e650f438748a
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-16 18:46:02 +0200

    4.01-strlen

---
+++ template/Chap4-Strings/4.01-strlen.c
@@ -0,0 +1,37 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 4.01-strlen.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 4.01
+                    Proposez une implémentation de la fonction strlen dont le
+                    prototype et la sémantique sont donnés ci-dessous :
+size_t strlen(const char* s);
+
+                    Semantique :
+                        Renvoie le nombre de caractères de la chaîne s
+                        (le '\0' final n'est pas comptabilisé)
+
+                    Ecrire aussi un petit programme permettant de tester votre
+                    fonction.
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t strlen(const char* s);
+
+int main(void) {
+    // <code>
+    return EXIT_SUCCESS;
+}
+
+size_t strlen(const char* s) {
+
+}

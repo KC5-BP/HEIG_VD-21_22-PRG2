416924d7c0e5b540bbaa4a7d661d60e3088baf08
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-16 18:48:01 +0200

    4.07-strchr

---
+++ template/Chap4-Strings/4.07-strchr.c
@@ -0,0 +1,38 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 4.07-strchr.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 4.07
+                    Proposez une implémentation de la fonction strchr dont le
+                    prototype et la sémantique sont donnés ci-dessous :
+char* strchr(const char* s, int c);
+
+                    Semantique :
+                        Retourne un pointeur sur la première occurrence du
+                        "caractère" c dans la chaîne s ou NULL si c n'est pas
+                        trouvé.
+
+                    Ecrire aussi un petit programme permettant de tester votre
+                    fonction.
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* strchr(const char* s, int c);
+
+int main(void) {
+	// <code>
+	return EXIT_SUCCESS;
+}
+
+char* strchr(const char* s, int c) {
+
+}

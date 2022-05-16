85ee5a4391ace6aaf295f31757305206b6c6885f
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-16 18:49:38 +0200

    4.08-Firstname & Name : Concatenate 2 user's input (a Name and a Firstname) into one string.

---
+++ template/Chap4-Strings/4.08-Names.c
@@ -0,0 +1,35 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : 4.08-Names.c
+ Auteur(s)      : (\_/)
+ Date creation  : <jj.mm.aaaa>
+
+ Description    : 4.08
+                    Ecrire une fonction C qui prend en paramètres un prénom et
+                    un nom et qui retourne une nouvelle chaîne sous la forme
+                    "prénom nom".
+
+                    Exemple :
+                        "James" + "Bond" => "James Bond".
+
+                    Ecrire aussi un petit programme de test qui, après avoir
+                    demandé à l'utilisateur d'entrer un prénom, puis un nom,
+                    affiche à l'écran, en utilisant les services de la
+                    fonction ci-dessus :
+                        La chaine "prenom nom" comporte n caracteres.
+                    Soit, dans notre exemple :
+                        La chaine "James Bond" comporte 10 caracteres.
+
+ Remarque(s)    : <� compl�ter>
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	// <code>
+	return EXIT_SUCCESS;
+}

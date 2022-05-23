4a3f22431411d481f50f53937fce67d6fd954405
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-05-23 13:44:02 +0200

    3.03-People Display : Display an array of people (struct type)

--- corr/Chap3-CompTypes/3.03-PeopleDisplay.c
+++ corr/Chap3-CompTypes/3.03-PeopleDisplay.c
@@ -2,7 +2,7 @@
  -----------------------------------------------------------------------------------
  Nom du fichier : 3.03-PeopleDisplay.c
  Auteur(s)      : (\_/)
- Date creation  : <j .mm.aaaa>
+ Date creation  : <jj.mm.aaaa>
 
  Description    : 3.03
                     Même exercice que précédemment, mais en affichant non pas une
@@ -31,6 +31,54 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
+typedef const char* Nom;
+typedef uint8_t Taille;
+typedef enum { BLUE, VERT, GRIS, MARRON, NOIR } CouleurYeux;
+
+typedef struct {
+    Nom nom;
+    Taille taille;  // en cm
+    CouleurYeux couleurYeux;
+} Personne;
+
+const char* const COULEURS_YEUX[] = {"blue", "vert", "gris", "marron", "noir"};
+
+void afficherPersonne(const Personne* p);
+void afficherPersonnes(const Personne tab[], size_t n);
+
 int main(void) {
-	return EXIT_SUCCESS;
+    const Personne PERSONNES[] = {{"Paul",        180, BLUE},
+                                  {"Pierre",      175, VERT},
+                                  {"Jean-Jaques", 182, MARRON}};
+    const size_t TAILLE = sizeof(PERSONNES) / sizeof(Personne);
+
+    afficherPersonnes(PERSONNES, TAILLE);
+
+    return EXIT_SUCCESS;
 }
+
+void afficherPersonne(const Personne* p) {
+    printf("Nom     : %s\n", p->nom);
+    printf("Taille  : %" PRIu8 "\n", p->taille);
+    printf("Couleur : %s\n", COULEURS_YEUX[p->couleurYeux]);
+}
+
+void afficherPersonnes(const Personne tab[], size_t n) {
+    for (size_t i = 0; i < n; ++i) {
+        afficherPersonne(tab + i);    // OU &tab[i]
+        printf("\n");
+    }
+}
+
+// Ouput :
+//Nom     : Paul
+//Taille  : 180
+//Couleur : blue
+//
+//Nom     : Pierre
+//Taille  : 175
+//Couleur : vert
+//
+//Nom     : Jean-Jaques
+//Taille  : 182
+//Couleur : marron

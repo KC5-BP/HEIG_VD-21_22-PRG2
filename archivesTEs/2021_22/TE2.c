c02395ea081f6ab2caf939f2c746d4c406460f62
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2022-06-06 11:39:34 +0200

    Clean 2021_22 while doing 2017_18

--- archivesTEs/2021_22/TE2.c
+++ archivesTEs/2021_22/TE2.c
@@ -18,156 +18,157 @@
 #include <string.h>
 #include <stdbool.h>
 #include <inttypes.h>
-#include <assert.h>
 
 #define ENDL(void) printf("\n")
 
 void exo1(void) {
-    {
-        printf("%s", "Exo 1) ------------------------------\n");
-        {
-            char chaine[3] = "ABC"; // Wrong, needed chaine[4]
-        }
-        {
-            char chaine[5];
-            //chaine = "ABC";   // Wrong, chaine is a cst ptr!
-                                // chaine[0] = 'A', chaine[1] = 'B',
-        }                       // chaine[2] = 'C' to be correct
-        {
-            const char* chaine1;
-            char chaine2[] = "ABC";
-            chaine1 = chaine2;  // Correct, chaine1 is a cst ptr
-        }                       // and not a ptr on cst
-        {
-            typedef char string[];  // Correct, it will be a : char[]
-        }
-        {
-            /*
-            struct S {
-                int n = 1;
-                double x = 2.5; // Wrong, can't initialize value
-                                // inside struct declaration
-            } s;
-             */
-        }
-        {
-            typedef struct {
-                const int N;    // Correct, const must be
-                double x;       // initialize at var. creation.
-            } S;
-            S s = {1, 2.5};
-        }
-        {
-            /*
-            struct S {
-                const int N;    // Wrong, const must be
-                double x;       // initialize at var. creation.
-            } s;
-            s = (struct S) {1, 2.5};
-             */
-        }
-        {
-            /*
-            struct S {
-                int n;
-                static int m;   // Wrong, can't specify a static inside a struct
-            };
-            struct S s = {1, 2};
-             */
-        }
-        {
-            struct S {
-                int n;          // Correct
-                double x;
-            };
-            struct S s1 = {.x = 2.5};
-            struct S s2 = s1;
-        }
-        {
-            struct S {
-                int n;          // Wrong, can't compare struct's
-                double x;
-            };
-            struct S s1 = {1, 2.5};
-            struct S s2 = {1, 2.5};
-            //printf("s1 == s2 ? %s\n", (s1 == s2 ? "oui" : "non"));
-        }
-        {
-            struct S {          // Correct, but careful, A starts at 0, so B = 1
-                enum {A, B, C = 0} e;
-            } s;
-            s.e = B;
-            printf("%d\n", s.e);
-        }
-        {
-            struct S1 {         // Correct because it's a pointer, kind of
-                struct S2* a;   // anticipated declaration
-            };
-            struct S2 {
-                struct S1* a;
-            };
-        }
-        {
-            /*
-            typedef struct S {
-                int n;
-                T* ptr;         // Wrong, T is not known until end of struct
-            } T;                // declaration
-            T t = {1, NULL};
-             */
-            // To make it possible :
-            //struct T;
-            //typedef struct S {
-            //    int n;
-            //    struct T* ptr;
-            //} T;
-            //T t = {1, NULL};
-        }
-        {
-            union U {
-                int n;
-                double x;       // Wrong, because without specifier, it's the first
-            };                  // var. that is initialized.
-            union U u = {1.5};
-            //union U u = {.x = 1.5};   // To be correct
-            printf("u = %f\n", u.x);
-        }
+    printf("%s", "Exo 1) ------------------------------\n");
+    {   // 1)
+        char chaine[3] = "ABC"; // Wrong, needed chaine[4]
+    }
+    {   // 2)
+        char chaine[5];
+        //chaine = "ABC";   // Wrong, chaine is a cst ptr!
+                            // chaine[0] = 'A', chaine[1] = 'B',
+    }                       // chaine[2] = 'C' to be correct
+    {   // 3)
+        const char* chaine1;
+        char chaine2[] = "ABC";
+        chaine1 = chaine2;  // Correct, chaine1 is a cst ptr
+    }                       // and not a ptr on cst
+    {   // 4)
+        typedef char string[];  // Correct, it will be a : char[]
+    }
+    {   // 5)
+        /*
+        struct S {
+            int n = 1;
+            double x = 2.5; // Wrong, can't initialize value
+                            // inside struct declaration
+        } s;
+         */
+    }
+    {   // 6)
+        typedef struct {
+            const int N;    // Correct, const must be
+            double x;       // initialize at var. creation.
+        } S;
+        S s = {1, 2.5};
+    }
+    {   // 7)
+        /*
+        struct S {
+            const int N;    // Wrong, const must be
+            double x;       // initialize at var. creation.
+        } s;
+        s = (struct S) {1, 2.5};
+         */
+    }
+    {   // 8)
+        /*
+        struct S {
+            int n;
+            static int m;   // Wrong, can't specify a static inside a struct
+        };
+        struct S s = {1, 2};
+         */
+    }
+    {   // 9)
+        struct S {
+            int n;          // Correct
+            double x;
+        };
+        struct S s1 = {.x = 2.5};
+        struct S s2 = s1;
+    }
+    {   // 10)
+        struct S {
+            int n;          // Wrong, can't compare struct's
+            double x;
+        };
+        struct S s1 = {1, 2.5};
+        struct S s2 = {1, 2.5};
+        //printf("s1 == s2 ? %s\n", (s1 == s2 ? "oui" : "non"));
+    }
+    {   // 11)
+        struct S {          // Correct, but careful, A starts at 0, so B = 1
+            enum {A, B, C = 0} e;
+        } s;
+        s.e = B;
+        printf("%d\n", s.e);
+    }
+    {   // 12)
+        struct S1 {         // Correct because it's a pointer
+            struct S2* a;   // But it will be Wrong if it missed
+        };                  // the keyword struct
+        struct S2 {
+            struct S1* a;
+        };
+    }
+    {   // 13)
+        /*
+        typedef struct S {
+            int n;
+            T* ptr;         // Wrong, T is not known until end of struct
+        } T;                // declaration
+        T t = {1, NULL};
+         */
+        // To make it possible :
+        //struct T;
+        //typedef struct S {
+        //    int n;
+        //    struct T* ptr;
+        //} T;
+        //T t = {1, NULL};
+    }
+    {   // 14)
+        union U {
+            int n;
+            double x;       // Wrong, because without specifier, it's the first
+        };                  // var. that is initialized.
+        union U u = {1.5};
+        //union U u = {.x = 1.5};   // To be correct
+        printf("u = %f\n", u.x);
     }
 }
 
 void exo2(void) {
-    printf("%s", "Exo 2a) -----------------------------\n");
-    const char* t[] = {"Fiat", "Peugeot", "Ford", "Volvo", "Lancia", "Jeep"};
-    const char** pt[] = {t, t + 1, t + 2, t + 3, t + 4, t + 5};
+    {
+        printf("%s", "Exo 2a) -----------------------------\n");
+        const char* t[] = {"Fiat", "Peugeot", "Ford", "Volvo", "Lancia", "Jeep"};
+        const char** pt[] = {t, t + 1, t + 2, t + 3, t + 4, t + 5};
 
-    printf("1) %d\n", *pt - pt[3]); // -3
-    printf("2) %s\n", *pt[2]);      // "Ford"
-    printf("3) %d (char : %c)\n", **(*pt+5) - 2, **(*pt+5) - 2);    // 72
-    printf("4) %s\n", *t+2);        // "at"
-    printf("5) %c\n", pt[++pt[3]-*(pt+3)][1][2]);   // 'u'
-    //      1)  *pt = t
-    //          pt[3] = t + 3
-    //          t - (t + 3) = -3
-    //      2)  pt[2] = t + 2
-    //          *(t + 2) = "Ford"
-    //      3)  *pt = t
-    //          t + 5 => &"Jeep"
-    //          **&"Jeep" = J
-    //          'J' - 2 => Conversion, so numeric value of 'H' = 72
-    //      4)  *t = "Fiat"
-    //          "Fiat" + 2 => "at"
-    //      5)  pt[3] = t + 3
-    //          ++(t+3) = t + 4     Leading to change the pointer in pt[3]
-    //          *(pt + 3) = t + 4
-    //          t + 4 - (t + 4) = 0
-    //          pt[0] = t => t[1] = "Peugeot" => "Peugeot"[2] = 'u'
-    printf("%s", "Exo 2b) -----------------------------\n");
-    char argv[6][20];
-    int argc = sscanf("myprog -opt1=10 test.dat 20 -opt2=src", "%s %s %s %s %s",
-                   argv[0], argv[1], argv[2], argv[3], argv[4]);
-    printf("argc = %d\n", argc);
-    for (int i = 0; i < argc; ++i)
-        printf("argv[%d] : %s\n", i, argv[i]);
+        printf("1) %d\n", *pt - pt[3]); // -3
+        printf("2) %s\n", *pt[2]);      // "Ford"
+        printf("3) %d (char : %c)\n", **(*pt + 5) - 2, **(*pt + 5) - 2);    // 72
+        printf("4) %s\n", *t + 2);      // "at"
+        printf("5) %c\n", pt[++pt[3] - *(pt + 3)][1][2]);   // 'u'
+        //      1)  *pt = t
+        //          pt[3] = t + 3
+        //          t - (t + 3) = -3
+        //      2)  pt[2] = t + 2
+        //          *(t + 2) = "Ford"
+        //      3)  *pt = t
+        //          t + 5 => &"Jeep"
+        //          **&"Jeep" = J
+        //          'J' - 2 => Conversion, so numeric value of 'H' = 72
+        //      4)  *t = "Fiat"
+        //          "Fiat" + 2 => "at"
+        //      5)  pt[3] = t + 3
+        //          ++(t+3) = t + 4     Leading to change the pointer in pt[3]
+        //          *(pt + 3) = t + 4
+        //          t + 4 - (t + 4) = 0
+        //          pt[0] = t => t[1] = "Peugeot" => "Peugeot"[2] = 'u'
+    }
+    {
+        printf("%s", "Exo 2b) -----------------------------\n");
+        char argv[6][20];
+        int argc = sscanf("myprog -opt1=10 test.dat 20 -opt2=src", "%s %s %s %s %s",
+                       argv[0], argv[1], argv[2], argv[3], argv[4]);
+        printf("argc = %d\n", argc);
+        for (int i = 0; i < argc; ++i)
+            printf("argv[%d] : %s\n", i, argv[i]);
+    }
 }
 
 // 3b)
@@ -267,7 +268,8 @@ void exo4(void) {
         printf("%s\n", strncpy(chaine1 + 1, chaine3, strlen(chaine2)));
         // chaine1 = "\0ABCX\0"
         // chaine1 + 1, so return and display "ABCX"
-
+    }
+    {
         printf("%s", "Exo 4b) -----------------------------\n");
         printf("Total errors : 6\n");
     }
